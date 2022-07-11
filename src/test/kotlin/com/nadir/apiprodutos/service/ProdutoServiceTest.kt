b23a2378a9a605a07708bfd5332a27e5dfb6a03d
Allan Santos <allannvictor@gmail.com>
2022-07-11 16:54:59 -0300

    active profile test

--- src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
+++ src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
@@ -14,9 +14,11 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
 class ProdutoServiceTest {
 
