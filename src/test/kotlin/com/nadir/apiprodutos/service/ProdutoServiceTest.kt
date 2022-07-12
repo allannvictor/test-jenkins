caf997b4db08648102d6c6330df673ed068f0a74
Allan Santos <allannvictor@gmail.com>
2022-07-12 12:14:07 -0300

    more tests

--- src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
+++ src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
@@ -88,88 +88,88 @@ class ProdutoServiceTest {
         assertEquals(product, produto)
     }
 
-//    @Test
-//    fun `quando desativa um produto este é desativado`() {
-//        produto = ProdutoComponent.createActiveProdutoEntity()
-//        produto.quantidade = BigDecimal.ZERO
-//        produto.quantidadeReservadaCarrinho = BigDecimal.ZERO
-//        val statusAnterior: Boolean = produto.isActive
-//        val statusPosterior: Boolean
-//        val id: Long = produto.id!!
-//
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
-//        `when`(produtoRepository.save(produto)).thenReturn(produto)
-//        statusPosterior = produtoService.disable(id).isActive
-//
-//        verify(produtoRepository, atLeastOnce()).findById(id)
-//        verify(produtoRepository, atLeastOnce()).save(produto)
-//
-//        assertEquals(true, statusAnterior)
-//        assertEquals(false, statusPosterior)
-//        assertNotEquals(statusPosterior, statusAnterior)
-//    }
-//
-//    @Test
-//    fun `quando desativa um produto com estoque não zerado espera mensagem de excecao`() {
-//        produto = ProdutoComponent.createActiveProdutoEntity()
-//        produto.quantidade = BigDecimal.TEN
-//        produto.quantidadeReservadaCarrinho = BigDecimal.ONE
-//        val id: Long = produto.id!!
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
-//        try{
-//            produtoService.disable(id).isActive
-//            verify(produtoRepository, atLeastOnce()).findById(id)
-//            verify(produtoRepository, atLeastOnce()).save(produto)
-//        } catch(ex: EstoqueNaoZeradoException) {
-//            assertEquals("O estoque precisa estar zerado para desativar o produto".format(id), ex.message)
-//        }
-//    }
-//
-//
-//    @Test
-//    fun `quando se tenta desativar com id inexistente lanca excecao NotFound`() {
-//        val id: Long = 10L
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.empty())
-//        assertThrows<NotFoundException> { produtoService.disable(id) }
-//    }
-//
-//    @Test
-//    fun `quando se tenta desativar com id inexistente lanca excecao`() {
-//        val id: Long = 10L
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.empty())
-//        try{
-//            produtoService.disable(id)
-//        } catch (ex: NotFoundException) {
-//            assertEquals("Produto com id %s não localizado.".format(id), ex.message)
-//        }
-//    }
-//
-//    @Test
-//    fun `quando ativa um produto este é ativado`() {
-//        produto = ProdutoComponent.createInactiveProdutoEntity()
-//        val statusAnterior: Boolean = produto.isActive
-//        val statusPosterior: Boolean
-//        val id: Long = produto.id!!
-//
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
-//        `when`(produtoRepository.save(produto)).thenReturn(produto)
-//        statusPosterior = produtoService.enable(id).isActive
-//
-//        verify(produtoRepository, atLeastOnce()).findById(id)
-//        verify(produtoRepository, atLeastOnce()).save(produto)
-//
-//        assertEquals(false, statusAnterior)
-//        assertEquals(true, statusPosterior)
-//        assertNotEquals(statusPosterior, statusAnterior)
-//    }
-//
-//    @Test
-//    fun `quando busca um produto por id este é encontrado`() {
-//        produto = ProdutoComponent.createActiveProdutoEntity()
-//        val id: Long = produto.id!!
-//        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
-//        var produtoARetornar = produtoService.findById(id)
-//        verify(produtoRepository, only()).findById(id)
-//        assertEquals(produto, produtoARetornar)
-//    }
+    @Test
+    fun `quando desativa um produto este é desativado`() {
+        produto = ProdutoComponent.createActiveProdutoEntity()
+        produto.quantidade = BigDecimal.ZERO
+        produto.quantidadeReservadaCarrinho = BigDecimal.ZERO
+        val statusAnterior: Boolean = produto.isActive
+        val statusPosterior: Boolean
+        val id: Long = produto.id!!
+
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
+        `when`(produtoRepository.save(produto)).thenReturn(produto)
+        statusPosterior = produtoService.disable(id).isActive
+
+        verify(produtoRepository, atLeastOnce()).findById(id)
+        verify(produtoRepository, atLeastOnce()).save(produto)
+
+        assertEquals(true, statusAnterior)
+        assertEquals(false, statusPosterior)
+        assertNotEquals(statusPosterior, statusAnterior)
+    }
+
+    @Test
+    fun `quando desativa um produto com estoque não zerado espera mensagem de excecao`() {
+        produto = ProdutoComponent.createActiveProdutoEntity()
+        produto.quantidade = BigDecimal.TEN
+        produto.quantidadeReservadaCarrinho = BigDecimal.ONE
+        val id: Long = produto.id!!
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
+        try{
+            produtoService.disable(id).isActive
+            verify(produtoRepository, atLeastOnce()).findById(id)
+            verify(produtoRepository, atLeastOnce()).save(produto)
+        } catch(ex: EstoqueNaoZeradoException) {
+            assertEquals("O estoque precisa estar zerado para desativar o produto".format(id), ex.message)
+        }
+    }
+
+
+    @Test
+    fun `quando se tenta desativar com id inexistente lanca excecao NotFound`() {
+        val id: Long = 10L
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.empty())
+        assertThrows<NotFoundException> { produtoService.disable(id) }
+    }
+
+    @Test
+    fun `quando se tenta desativar com id inexistente lanca excecao`() {
+        val id: Long = 10L
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.empty())
+        try{
+            produtoService.disable(id)
+        } catch (ex: NotFoundException) {
+            assertEquals("Produto com id %s não localizado.".format(id), ex.message)
+        }
+    }
+
+    @Test
+    fun `quando ativa um produto este é ativado`() {
+        produto = ProdutoComponent.createInactiveProdutoEntity()
+        val statusAnterior: Boolean = produto.isActive
+        val statusPosterior: Boolean
+        val id: Long = produto.id!!
+
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
+        `when`(produtoRepository.save(produto)).thenReturn(produto)
+        statusPosterior = produtoService.enable(id).isActive
+
+        verify(produtoRepository, atLeastOnce()).findById(id)
+        verify(produtoRepository, atLeastOnce()).save(produto)
+
+        assertEquals(false, statusAnterior)
+        assertEquals(true, statusPosterior)
+        assertNotEquals(statusPosterior, statusAnterior)
+    }
+
+    @Test
+    fun `quando busca um produto por id este é encontrado`() {
+        produto = ProdutoComponent.createActiveProdutoEntity()
+        val id: Long = produto.id!!
+        `when`(produtoRepository.findById(id)).thenReturn(Optional.of(produto))
+        var produtoARetornar = produtoService.findById(id)
+        verify(produtoRepository, only()).findById(id)
+        assertEquals(produto, produtoARetornar)
+    }
 }
