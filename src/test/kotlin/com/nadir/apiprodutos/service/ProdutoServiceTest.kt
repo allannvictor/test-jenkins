c0046b514007b156ea60f493005d30c2ec83823a
Allan Santos <allannvictor@gmail.com>
2022-07-12 10:59:24 -0300

    context configuration

--- src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
+++ src/test/kotlin/com/nadir/apiprodutos/service/ProdutoServiceTest.kt
@@ -15,10 +15,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ContextConfiguration
 @ExtendWith(MockKExtension::class)
 class ProdutoServiceTest {
 
